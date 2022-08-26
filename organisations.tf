4ac4971110f01bbffc60869ba2f2534127b0ddf4
Paul Hardy <Paul.Hardy@globalpay.com>
2022-08-26 21:52:03 +0100

    Bugfix

--- organisations.tf
+++ organisations.tf
@@ -1,16 +1,28 @@
 resource "aws_organizations_organization" "root" {
 }
 
-resource "aws_organizations_account" "users" {
+resource "aws_organizations_account" "standard" {
   for_each = var.accounts
   name      = "${var.name}-${each.value}"
   email     = "admin+${each.value}@${var.email_domain}"
   role_name = "Admin"
 }
 
-resource "aws_organizations_account" "users" {
+resource "aws_organizations_account" "project" {
   for_each = var.accounts
   name      = "${var.name}-${each.value}"
   email     = "admin+${each.value}@${var.email_domain}"
   role_name = "Admin"
+}
+
+module "configure_iam" {
+  for_each = aws_organizations_account.standard
+  source = "./modules/"
+  provider "aws" {
+    assume_role {
+      role_arn = "arn:aws:iam::${each.value.id}:role/Admin"
+    }
+    alias  = each.value.id
+    region = var.region
+  }
 }
